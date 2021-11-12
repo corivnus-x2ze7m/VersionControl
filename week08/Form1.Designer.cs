c2e7fef077670976a2594bfa1c12845111ded34b
corvinus-x2ze7m <rejhoka@gmail.com>
2021-11-12 23:08:28 +0100

    F3

--- week08/Form1.Designer.cs
+++ week08/Form1.Designer.cs
@@ -29,21 +29,49 @@ namespace week08
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.creteaTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 359);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 18);
+            this.panel1.TabIndex = 0;
+            // 
+            // creteaTimer
+            // 
+            this.creteaTimer.Enabled = true;
+            this.creteaTimer.Interval = 3000;
+            this.creteaTimer.Tick += new System.EventHandler(this.creteaTimer_Tick);
+            // 
+            // conveyTimer
+            // 
+            this.conveyTimer.Enabled = true;
+            this.conveyTimer.Interval = 10;
+            this.conveyTimer.Tick += new System.EventHandler(this.conveyTimer_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(805, 542);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer creteaTimer;
+        private System.Windows.Forms.Timer conveyTimer;
     }
 }
 
--- week08/Form1.cs
+++ week08/Form1.cs
@@ -12,14 +12,51 @@ namespace week08
 {
     public partial class Form1 : Form
     {
+
+        private List<Entities.Ball> _balls = new List<Entities.Ball>();
+
+        private Entities.BallFactory _factory;
+        public Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new Entities.BallFactory();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void conveyTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+
+        private void creteaTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
         }
     }
 }
