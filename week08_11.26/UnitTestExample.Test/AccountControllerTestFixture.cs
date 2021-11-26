07d4616e8436fd72f53911727921de82836173cf
corvinus-x2ze7m <rejhoka@gmail.com>
2021-11-26 12:23:45 +0100

    T6

--- week08_11.26/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week08_11.26/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
         
-        [Test]
+        [Test, 
+            TestCase("abcd2134", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu",false),
+            TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidate(string email, bool expectedResult)
         {
             //Arrange
